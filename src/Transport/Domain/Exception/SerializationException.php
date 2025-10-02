09634810a00de3422db35ffc220c0613c89c7fa7
Oleg Patsay <smmartbiz@gmail.com>
2025-10-02 10:57:50 +0300

    FEAT: Added a SerializationException to handle serialization/deserialization errors (JSON encoding/decoding)

---
+++ src/Transport/Domain/Exception/SerializationException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tenqz\Qdrant\Transport\Domain\Exception;
+
+/**
+ * Exception for serialization/deserialization errors (JSON encode/decode)
+ */
+class SerializationException extends TransportException
+{
+}
